ce576b40e36c8e200a0d977a95a0000d503e1832
Luca Capra <luca.capra@gmail.com>
2016-07-30 11:28:22 +0200

    initial query interface implementation for sqlite

--- db/DeviceManagerDataset.go
+++ db/DeviceManagerDataset.go
@@ -9,20 +9,13 @@ import (
 )
 
 const (
-	defaultFilePath  = "./data.db"
-	defaultTableName = "Devices"
-)
+	// DatabasePath path to the database file
+	DatabasePath = "./data.db"
+	// TableName Name of the table containing the devices
+	TableName = "Devices"
 
-// DeviceManagerDataset a sqlite based dataset
-type DeviceManagerDataset struct {
-	db *sql.DB
-}
-
-// Setup a database connection
-func (d *DeviceManagerDataset) Setup() {
-
-	sqlStmt := `
-  create table ` + defaultTableName + ` (
+	createTableStmt = `
+  create table ` + TableName + ` (
     id integer not null primary key,
 		Id          text,
 		Name        text,
@@ -33,16 +26,28 @@ func (d *DeviceManagerDataset) Setup() {
 		Streams     text
   );
   `
-	_, err := d.db.Exec(sqlStmt)
+)
+
+// DeviceManagerDataset a sqlite based dataset
+type DeviceManagerDataset struct {
+	db *sql.DB
+}
+
+// Open setup internally for a database connection
+func (d *DeviceManagerDataset) Open() {
+
+	db, err := sql.Open("sqlite3", DatabasePath)
+	util.CheckError(err)
+	d.db = db
+
+	_, err = d.db.Exec(createTableStmt)
 	util.CheckError(err)
 
 }
 
 // Connect return a connection to the database
 func (d *DeviceManagerDataset) Connect() {
-	db, err := sql.Open("sqlite3", defaultFilePath)
-	util.CheckError(err)
-	d.db = db
+
 }
 
 // Disconnect close the db connection
@@ -52,14 +57,21 @@ func (d *DeviceManagerDataset) Disconnect() {
 
 // GetBy get records for a field / value match
 func (d *DeviceManagerDataset) GetBy(key string, id string) *sql.Result {
-	result, err := d.db.Exec("SELECT * FROM "+defaultTableName+" WHERE $1 = $2", key, id)
+	result, err := d.db.Exec("SELECT * FROM "+TableName+" WHERE $1 = $2", key, id)
 	util.CheckError(err)
 	return &result
 }
 
 // Find records in the db
 func (d *DeviceManagerDataset) Find(q api.Query) *sql.Result {
-	result, err := d.db.Exec("SELECT * FROM "+defaultTableName+" WHERE Id = $1 ORDER BY $2 $3", q.Criteria, q.OrderBy, q.Sort)
+	var where = ""
+	var args = make([]interface{}, len(q.Criteria))
+	for i, c := range q.Criteria {
+		p := string(i + 1)
+		where += c.Prefix + c.Field + c.Operation + "$" + p + c.Suffix
+		args = append(args, c.Value)
+	}
+	result, err := d.db.Exec("SELECT * FROM "+TableName+" WHERE "+where+" ORDER BY $2 $3", q.Criteria, q.OrderBy.Field, q.OrderBy.Sort)
 	util.CheckError(err)
 	return &result
 }
